f6dc6047ea3ad2c0db5687c7d038cd8f560431b5
Ulas Akdeniz <akdenizulas@gmail.com>
2017-07-07 19:36:38 +0200

    Changes http constant values

--- src/main/java/com/github/anlcnydn/Constants.java
+++ src/main/java/com/github/anlcnydn/Constants.java
@@ -2,9 +2,9 @@ package com.github.anlcnydn;
 
 public class Constants {
   //HTTP Results
-  public static final String OK = "ok";
+  public static final String OK = "Ok";
   public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
-  public static final String UNAUTHORIZED = "unauthorized";
+  public static final String UNAUTHORIZED = "Unauthorized";
   public static final String BAD_REQUEST = "Bad Request";
 
   public static final String URL = "https://graph.facebook.com/v2.6/me/messages?access_token=";
